93d7235acaf864b25f336dd963232dddb2264db2
Robert Picard <mail@robert.io>
2014-06-06 15:04:54 -0700

    Improve wrapper

--- js/wrapper.js
+++ js/wrapper.js
@@ -5,19 +5,27 @@ var curve25519 = (function() {
     function _allocate(bytes) {
         var address = Module._malloc(bytes.length);
         Module.HEAPU8.set(bytes, address);
+        
         return address;
+    }
+
+    function _readBytes(address, length) {
+        var bytes = new Uint8Array(length);
+        bytes.set(Module.HEAPU8.subarray(address, address + length));
+
+        return bytes;
     }
 
     /*
     * Expects: Uint8Array(32)
     * Returns: Uint8Array(32)
     */
-    function getPublicFromSecret (secret) {
+    function getPublicFromPrivate (privateKey) {
         // Where to store the result
         publicKey_ptr = Module._malloc(32);
 
-        // Get a pointer to the secret key
-        secretKey_ptr = _allocate(secret);
+        // Get a pointer to the private key
+        privateKey_ptr = _allocate(privateKey);
 
         // The basepoint for generating public keys is 0x09 followed by 31 null bytes
         baseArray = ["0x09"];
@@ -29,33 +37,33 @@ var curve25519 = (function() {
         basepoint_ptr = _allocate(new Uint8Array(baseArray));
 
         // The return value is just 0, the operation is done in place
-        err = Module._curve25519_donna(publicKey_ptr, secretKey_ptr, basepoint_ptr);
+        err = Module._curve25519_donna(publicKey_ptr, privateKey_ptr, basepoint_ptr);
 
         // Voilà
-        return publicKey_ptr;
+        return _readBytes(publicKey_ptr, 32);
     }
 
     /*
     * Expects: Uint8Array(32), Uint8Array(32)
     * Returns: Uint8Array(32)
     */
-    function getSharedSecret (mySecret, theirPublic) {
+    function getSharedSecret (myPrivate, theirPublic) {
         // Where to store the result
         sharedKey_ptr = Module._malloc(32);
 
-        // Get a pointer to our secret key
-        secretKey_ptr = _allocate(mySecret);
+        // Get a pointer to our private key
+        privateKey_ptr = _allocate(myPrivate);
 
         // Get a pointer to their public key, the basepoint when you're generating a shared secret
         basepoint_ptr = injectBytes(hexStringToByteArray(theirPublic));
 
         // Return value is 0 here too of course
-        err = Module._curve25519_donna(sharedKey_ptr, secretKey_ptr, basepoint_ptr);
+        err = Module._curve25519_donna(sharedKey_ptr, privateKey_ptr, basepoint_ptr);
 
-        return sharedKey_ptr;
+        return _readBytes(sharedKey_ptr, 32);
     }
 
-    exports.getPublicFromSecret = getPublicFromSecret;
+    exports.getPublicFromPrivate = getPublicFromPrivate;
     exports.getSharedSecret = getSharedSecret;
 
     return exports;
